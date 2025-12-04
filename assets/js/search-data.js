fd268b7d5cfc0410d8b3232b182279c4be3bcc5e
mobilescape <jychoi@dankook.ac.kr>
2025-12-04 10:03:20 +0000

    Deploying to gh-pages from @ mobilescape/mobilescape@b9baaf9b443614dfe43939299fe6e101ca974e3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mobilescape/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/mobilescape/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/mobilescape/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
