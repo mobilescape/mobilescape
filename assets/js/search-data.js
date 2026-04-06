bbc17681b657a4ea99ef2e525fd5ebe45856702d
mobilescape <jychoi@dankook.ac.kr>
2026-04-06 15:43:28 +0000

    Deploying to gh-pages from @ mobilescape/mobilescape@22990e9c028828f532fa673dce418d4004e73dae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,16 +397,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/mobilescape/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/mobilescape/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-kim-geunseon-and-jung-yubin-have-joined-mobilescape-as-undergraduate-researchers",
+          title: 'KIM Geunseon and JUNG Yubin have joined Mobilescape as undergraduate researchers.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
